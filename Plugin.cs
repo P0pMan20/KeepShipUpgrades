a9ab829815124c93c539c039275f489ef58620cf
P0pMan20 <51844807+P0pMan20@users.noreply.github.com>
2023-12-07 21:39:19 +1000

    inital commit

---
+++ Plugin.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+
+namespace KeepShipUpgrades
+{
+    [BepInPlugin("pop.mods.keepshipupgrades", "KeepShipUpgrades", "0.0.1")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"KeepShipUpgrades has loaded :)");
+        }
+    }
+}
