f844f3d47b086303ef9cf071380c4a62f61b157b
P0pMan20 <51844807+P0pMan20@users.noreply.github.com>
2023-12-09 17:22:41 +1000

    hopefully this works

--- Plugin.cs
+++ Plugin.cs
@@ -1,4 +1,6 @@
-﻿using BepInEx;
+﻿using System.Collections.Generic;
+using System.Net.Http.Headers;
+using BepInEx;
 using HarmonyLib;
 using UnityEngine;
 
@@ -13,83 +15,51 @@ namespace KeepShipUpgrades
             // Plugin startup logic
             Logger.LogInfo($"KeepShipUpgrades has loaded :)");
             Harmony.CreateAndPatchAll(typeof(EnsureShipUpgradesAreKept));
+            Logger.LogInfo($"KeepShipUpgrades is finished patching :D");
+
         }
     }
 }
 
 class EnsureShipUpgradesAreKept
 {
-    // this is really shit and should be rewritten to be more easily updatable
-    // and less copyright infringing
-    // and less fuck up others mods
-    // it literally just copies the original code (from ILSpy) and removes all the shit that deletes upgrades
-    // We also skip any prefix that comes after this mod
-    // ideally you would copy the list of bought unlockables (ie loud horn, tv and pumpkin) before removal
-    // and give them back in a postfix
-    // might do it tomorrow
-    [HarmonyPatch(typeof(StartOfRound), "ResetShip")]
+    private static List<UnlockableItem> backupList;
+    private static int[] backupUnlockedShipObjects;
+
+    [HarmonyPatch(typeof(GameNetworkManager), "ResetSavedGameValues")]
     [HarmonyPrefix]
-    static bool SaveUpgrades(ref StartOfRound __instance)
+    static void SaveUpgradesBeforeTheyGetDeleted(ref GameNetworkManager __instance)
     {
-        TimeOfDay.Instance.globalTime = 100f;
-        TimeOfDay.Instance.profitQuota = TimeOfDay.Instance.quotaVariables.startingQuota;
-        TimeOfDay.Instance.quotaFulfilled = 0;
-        TimeOfDay.Instance.timesFulfilledQuota = 0;
-        TimeOfDay.Instance.timeUntilDeadline = (int)(TimeOfDay.Instance.totalTime * (float)TimeOfDay.Instance.quotaVariables.deadlineDaysAmount);
-        TimeOfDay.Instance.UpdateProfitQuotaCurrentTime();
-        __instance.randomMapSeed++;
-        Debug.Log("Reset ship 0");
-        __instance.companyBuyingRate = 0.3f;
-        __instance.ChangeLevel(__instance.defaultPlanet);
-        __instance.ChangePlanet();
-        __instance.SetMapScreenInfoToCurrentLevel();
-        Terminal terminal = Object.FindObjectOfType<Terminal>();
-        if (terminal != null)
-        {
-            terminal.groupCredits = TimeOfDay.Instance.quotaVariables.startingCredits;
-        }
-        if (__instance.IsServer)
-        {
-            RoundManager.Instance.DespawnPropsAtEndOfRound(despawnAllItems: true);
-            __instance.closetLeftDoor.SetBoolOnClientOnly(setTo: false);
-            __instance.closetRightDoor.SetBoolOnClientOnly(setTo: false);
-        }
-        Debug.Log("Reset ship A");
-        for (int l = 0; l < __instance.allPlayerScripts.Length; l++)
-        {
-            SoundManager.Instance.playerVoicePitchTargets[l] = 1f;
-            __instance.allPlayerScripts[l].ResetPlayerBloodObjects();
-            UnlockableSuit.SwitchSuitForPlayer(__instance.allPlayerScripts[l], 0);
-        }
-        Debug.Log("Reset ship D");
-        TimeOfDay.Instance.OnDayChanged();
-        // foreach (var locker in __instance.SpawnedShipUnlockables)
-        // {
-        //     Debug.Log();
-        // }
-        //
-        //
-        // foreach (var item in __instance.unlockablesList.unlockables)
-        // {
-        //     foreach (var field in item.GetType().GetFields())
-        //     {
-        //         Debug.Log($"{field.Name} F {field.GetValue(item)}");
-        //     }
-        // }
-        return false;
+        backupList = StartOfRound.Instance.unlockablesList.unlockables;
+        backupUnlockedShipObjects = ES3.Load<int[]>("UnlockedShipObjects", GameNetworkManager.Instance.currentSaveFileName);
+
     }
     
-    // // skip reset
-    // [HarmonyPatch(typeof(GameNetworkManager), "ResetUnlockablesListValues")]
-    // [HarmonyPrefix]
-    // static bool GameNoReset(ref GameNetworkManager __instance)
-    // {
-    //     return true;
-    // }    
-    // [HarmonyPatch(typeof(RoundManager), "DespawnPropsAtEndOfRound")]
-    // [HarmonyPrefix]
-    // static bool NoDeez(ref RoundManager __instance)
-    // {
-    //     return true;
-    // }
+    
+    // hopefully this all syncs across from the server to the clients
+    [HarmonyPatch(typeof(StartOfRound), "EndPlayersFiredSequenceClientRpc")]
+    [HarmonyPrefix]
+    static void ReinstateUpgrades(ref StartOfRound __instance)
+    {
+        ES3.Save<int[]>("UnlockedShipObjects", backupUnlockedShipObjects, GameNetworkManager.Instance.currentSaveFileName);
+        __instance.unlockablesList.unlockables = backupList;
+        Debug.Log(backupList==__instance.unlockablesList.unlockables);
+        Debug.Log(backupList==StartOfRound.Instance.unlockablesList.unlockables);
+        for (int i = 0; i < __instance.unlockablesList.unlockables.Count; i++)
+        {
+            // I believe this is if moveable item and the other type is probably the suits
+            // yes see StartOfRound.SpawnUnlockable https://discord.com/channels/926749935263698964/926749935263698967/1182945048254480384
+            if (__instance.unlockablesList.unlockables[i].unlockableType == 1)
+            {
+                string itemName = __instance.unlockablesList.unlockables[i].unlockableName;
+                ES3.Save("ShipUnlockMoved_"  + itemName , __instance.unlockablesList.unlockables[i].hasBeenMoved, GameNetworkManager.Instance.currentSaveFileName);
+                ES3.Save("ShipUnlockStored_" + itemName, __instance.unlockablesList.unlockables[i].inStorage, GameNetworkManager.Instance.currentSaveFileName);
+                ES3.Save("ShipUnlockPos_" + itemName, __instance.unlockablesList.unlockables[i].placedPosition, GameNetworkManager.Instance.currentSaveFileName);
+                ES3.Save("ShipUnlockRot_" + itemName, __instance.unlockablesList.unlockables[i].placedRotation, GameNetworkManager.Instance.currentSaveFileName);
+            }
+        }
+    }
+    
+
+
 }
